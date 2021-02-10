8c7b904f1c89acb8ff29d8ce706bf92cee5bcc4a
awenelo <56133692+awenelo@users.noreply.github.com>
2021-02-10 14:03:57 -0500

    Add extension parameter to POST send and handle received messages

--- popup.js
+++ popup.js
@@ -13,11 +13,19 @@ window.onload = function(element) {
     let loginDiv = document.getElementById('logindiv');
     let loggedInDiv = document.getElementById('loggedindiv');
     let errorDiv = document.getElementById('errordiv');
+    let messageP = document.getElementById('message');
     document.getElementById('submitdistance').addEventListener("click", async () => {
         var xmlHttp = new XMLHttpRequest();
+        xmlHttp.onreadystatechange = function() { 
+            if (xmlHttp.readyState == 4) {
+                messageP.textContent = xmlHttp.responseText;
+                show(messageP);
+                console.log(xmlHttp.responseText);
+            }
+        }
         xmlHttp.open("POST", "https://wciwalks.herokuapp.com/users/", true);
         xmlHttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-        xmlHttp.send("distance=".concat(document.getElementById('distance').value).concat("&submit=Submit"));
+        xmlHttp.send("distance=".concat(document.getElementById('distance').value).concat("&submit=Submit&extension=1"));
     });
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() { 
